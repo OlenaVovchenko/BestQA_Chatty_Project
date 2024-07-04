159504f9449d400591384b6a6d8f174fee526b9b
OlenaVovchenko <olenavovchenko@gmail.com>
2024-07-04 21:18:20 +0200

    Add test folder to tracking

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy (xpath = "//*[@name=\"email\"]")
+    private WebElement emailEditBox;
+}
