7ea156497eae53ad4a3dbded2de65121fcc77c82
Olgayakubovich81 <olgale4@gmail.com>
2024-07-15 22:28:01 +0200

    Add LatestVersion

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,53 +1,22 @@
 package baseTest;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import drivers.ChromeRegistry;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import util.ScreenshotUtil;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 public class BaseTest {
-    protected static WebDriver driver;
-
-    public void maximizeBrowserWindow(WebDriver driver) {
+    protected WebDriver driver;
+    @BeforeEach
+    public void setUp() {
+        ChromeRegistry chromeRegistry = new ChromeRegistry();
+        driver = chromeRegistry.registerDriver();
         driver.manage().window().maximize();
     }
 
-//    @BeforeAll
-//    public static void startBeforeAllTests(WebDriver driver) {
-//        driver.manage().window().maximize();
-////        driver.get("http://chatty.telran-edu.de:8089/login");
-//    }
-
-//    @BeforeEach
-//    public void startEachTest() {
-////       driver.get("http://chatty.telran-edu.de:8089/login");
-
-//}
-
-
-//    @AfterAll
-//    public static void quitAfterAllTests() {
-//        driver.quit();
-//    }
-
-
-    //    public void defineTestResultTrue(boolean condition) {
-//        try {
-//            assertTrue(condition);
-//        } catch (AssertionError e) {
-//            ScreenshotUtil.captureScreen(driver, this.getClass().getName());
-//            fail("TEST FAILED: " + e.getMessage()); // Передача сообщения об ошибке в fail
-//        }
-//    }
-    public void defineTestResultTrue(boolean condition) {
-        try {
-            assertTrue(condition, "Expected condition to be true.");
-        } catch (AssertionError e) {
-            ScreenshotUtil.captureScreen(driver, this.getClass().getSimpleName());
-            throw e;  // Переброс исключения после логирования и создания скриншота
-        }
-
+    @AfterEach
+    public void quit() {
+        driver.quit();
     }
 }
